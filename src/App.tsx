ae4cf3d49b47f08aa3509a6074e83e1d1119031c
a-k-i-v-i <kiviasanova@gmail.com>
2023-03-10 07:45:21 -0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from "react"
 import './App.css';
 import Header from './components/header/Header';
 import {Routes, Route} from 'react-router-dom'
-import Admin from './components/admin/Admin';
+import Admin from './components/admin/Order';
 import Menu from './components/menu/Menu';
-import Order from './components/order/Order';
+import Order from './components/order/Admin';
 import { IProduct } from "./ProductTypes";
 
 function App() {
