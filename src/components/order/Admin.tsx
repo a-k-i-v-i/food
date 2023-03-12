3e78ba9733f2813a072078577a8440ba7239a9f0
a-k-i-v-i <kiviasanova@gmail.com>
2023-03-12 10:50:47 -0700

    push

--- src/components/order/Admin.tsx
+++ src/components/order/Admin.tsx
@@ -1,34 +1,67 @@
+import { useState } from "react";
 import { useTypedDispatch } from "../../hooks/useDispatch";
-import { useTypedSelector } from "../../hooks/useSelector";
+import uniqid from "uniqid";
 
+const Order = () => {
+  const fileReader = new FileReader();
 
-const Admin = ()=>{
-    const {order} = useTypedSelector((s)=> s.product)
-    const {increaseQ} = useTypedDispatch()
-    const {decreaseQ} = useTypedDispatch()
-    const {deleteFromOrder} = useTypedDispatch()
-    console.log(order);
-    return(
-        <section id="order">
-           <div className="container">
-            <div className="order">
-            {
-                order.map((el)=> (
-                    <div className="order">
-                        <img src={el.img} alt="" />
-                        <h1>{el.name}</h1>
-                        <h2>{el.price}</h2>
-                        <span onClick={()=> decreaseQ(el.id)}> - </span>
-                        <h3>QUANTITY: {el.quantity}</h3>
-                        <span onClick={()=> increaseQ(el.id)}> + </span>
-                        <button onClick={()=> deleteFromOrder(el)}>DELETE ORDER</button>
-                    </div>
-                ))
-            }
-            </div>
-           </div>
-        </section>
-    )
-}
+  const { addToProduct } = useTypedDispatch();
+  const [fileUrl, setFileUrl] = useState<ArrayBuffer | null | string>("");
+  const [value, setValue] = useState<any>({
+    name: "",
+    price: "",
+    img: fileUrl,
+    id: uniqid(),
+  });
+  fileReader.onloadend = () => {
+    setFileUrl(fileReader.result);
+  };
+  const getValue = (e: any) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
 
-export default Admin
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
+  const handleChangeUrl = (e: React.ChangeEvent<any>) => {
+    fileReader.readAsDataURL(e.target.files[0]);
+  };
+  console.log(fileUrl);
+
+  return (
+    <section id="admin">
+      <div className="container">
+        <div className="admin">
+          <h1 className="admin--title">CREATE PRODUCT</h1>
+          <form onSubmit={handleSubmit} action="">
+            <input 
+            className="file--input"
+            onChange={handleChangeUrl} type="file" />
+            <input
+            className="name--input"
+              onChange={getValue}
+              name="name"
+              type="text"
+              placeholder="food--name"
+            />
+            <input
+            className="price--input"
+              onChange={getValue}
+              name="price"
+              type="text"
+              placeholder="price"
+            />
+            <button 
+            className="form--btn"
+            onClick={() => addToProduct({ ...value, img: fileUrl })}>
+              CREATE
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Order;
